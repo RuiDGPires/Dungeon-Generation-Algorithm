155918b3c1c2649d568b40650e2ececedd6dfd31
RuiDGPires <RuiDGPires@users.noreply.github.com>
2021-07-16 12:56:22 +0100

    Added dragging and zooming to test

--- test.gd
+++ test.gd
@@ -18,11 +18,12 @@ func _ready():
 	update()
 	
 
-const cell_size = 11
-const offset = Vector2(20, 10)
+var cell_size = 11
+var base_offset = Vector2(20, 10)
+var offset = base_offset
 
 func _draw():
-	var colors = [Color(0,0,0), Color(0, 0.98, 0.32), Color(0.88, 0.78, 0), Color(0.9, 0.9, 0.14)]
+	var colors = [Color(0,0,0,0), Color(0, 0.98, 0.32), Color(0.88, 0.78, 0), Color(0.9, 0.9, 0.14)]
 
 	for x in range(dun.map.size.x):
 		for y in range(dun.map.size.y):
@@ -32,6 +33,9 @@ func _draw():
 func pointToScreen(point: Vector2) -> Vector2:
 	return offset + point*cell_size
 
+var dragging = false
+var pos: Vector2
+
 func _input(event):
 	if event.is_action_pressed("restart"):
 		var rng = RandomNumberGenerator.new()
@@ -39,4 +43,27 @@ func _input(event):
 		
 		dun = Dungeon.new(min_size, max_size, number_of_rooms, min_room_size, max_room_size, rng)
 		
+		update()
+	
+	
+	if event.is_action_pressed("scroll_up") and event.control:
+		cell_size += 1
+		update()
+	elif event.is_action_pressed("scroll_down") and event.control:
+		cell_size -= 1
+		update()
+
+
+	if event.is_action_pressed("mouse_click"):
+		dragging = true
+		pos = event.position
+	if event.is_action_released("mouse_click"):
+		base_offset = offset
+		dragging = false
+		
+func  _process(delta):
+	if dragging:
+		var current = get_viewport().get_mouse_position()
+
+		offset = base_offset + current - pos
 		update()
